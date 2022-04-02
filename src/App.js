c692d0b842f8dceb79550d4432740aee517f5fb0
joewarsito <joewarsito@gmail.com>
2022-04-02 14:21:11 +0700

    beginning add sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import React from "react";
+
+//Components
+import Header from "./components/Header";
 
 function App() {
+    // Fetch Food
+    // const fetchFoods = async () => {
+    //   setLoading(true);
+  
+    //   try {
+    //     await getFoods().then(({ data }) => {
+    //       setSavedFoods(data.data);
+    //       dispatch({ type: "SET_USERS", data: data.data });
+    //     });
+    //   } catch (err) {
+    //     MySwal.fire({
+    //       icon: "error",
+    //       title: "Failed to fetch users."
+    //     });
+    //   } finally {
+    //     setTimeout(() => {
+    //       setLoading(false);
+    //     }, 500);
+    //   }
+    // };
+  
+    // useEffect(() => {
+    //   fetchFoods();
+    // }, []);
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
