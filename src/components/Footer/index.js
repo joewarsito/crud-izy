d70b63550ebe60e97dd0e9d706268f9415ba7720
joewarsito <joewarsito@gmail.com>
2022-04-02 14:35:49 +0700

    add components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Styles
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <span className="copyright">&copy; Food App by Jo</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Styles
+import "./style.scss";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <a href="/" className="logo">
+                    Food
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
