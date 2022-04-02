7c23972dca2173215956971db26b75002727c315
joewarsito <joewarsito@gmail.com>
2022-04-02 14:35:29 +0700

    beginning add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 //Components
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     // Fetch Food
@@ -31,6 +32,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
